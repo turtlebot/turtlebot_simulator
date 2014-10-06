bb58feb542db8cf464bfc5778ec34bcae29202fe
Mehdi Tlili <mehdi.tlili@gmail.com>
2014-10-06 12:14:22 +0900

    fix laser scan min max height for simulation

--- turtlebot_stdr/nodes/tf_connector.py
+++ turtlebot_stdr/nodes/tf_connector.py
@@ -11,12 +11,14 @@ class Remapper(object):
 
     def tf_remapper(self, msg):
 
+            a = 1
             if msg.transforms[0].header.frame_id == "/robot0":
                 self.br.sendTransform((0, 0, 0),
                                       tf.transformations.quaternion_from_euler(0, 0, 0),
                                       rospy.Time.now(),
                                       "base_footprint",
                                       "robot0")
+
 if __name__ == '__main__':
     rospy.init_node('remapper_nav')
     remapper = Remapper()
