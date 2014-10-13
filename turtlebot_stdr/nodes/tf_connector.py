06c1edf42344e32c2ff32df76aa6e31c1f49aad4
Mehdi Tlili <mehdi.tlili@gmail.com>
2014-10-13 13:00:05 +0900

    fixed map and costmap misalignement due to wrong frame_id

--- turtlebot_stdr/nodes/tf_connector.py
+++ turtlebot_stdr/nodes/tf_connector.py
@@ -2,25 +2,27 @@
 __author__ = 'mehdi tlili'
 import rospy
 from tf2_msgs.msg import TFMessage
+from nav_msgs.msg import OccupancyGrid
 import tf
 
 class Remapper(object):
 
     def __init__(self):
         self.br = tf.TransformBroadcaster()
+        self.map_pub = rospy.Publisher("new_map", OccupancyGrid, queue_size=10)
+        rospy.Subscriber("/tf", TFMessage, self.tf_remapper)
 
     def tf_remapper(self, msg):
 
-            a = 1
-            if msg.transforms[0].header.frame_id == "/robot0":
-                self.br.sendTransform((0, 0, 0),
-                                      tf.transformations.quaternion_from_euler(0, 0, 0),
-                                      rospy.Time.now(),
-                                      "base_footprint",
-                                      "robot0")
+        if msg.transforms[0].header.frame_id == "/robot0":
+            self.br.sendTransform((0, 0, 0),
+                                  tf.transformations.quaternion_from_euler(0, 0, 0),
+                                  rospy.Time.now(),
+                                  "base_footprint",
+                                  "robot0")
+
 
 if __name__ == '__main__':
     rospy.init_node('remapper_nav')
     remapper = Remapper()
-    rospy.Subscriber("/tf", TFMessage, remapper.tf_remapper)
     rospy.spin()
